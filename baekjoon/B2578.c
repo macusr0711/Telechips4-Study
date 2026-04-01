6540b27061bdc5b2f4dbcff36e4960a3c4ea80b3
macusr0711 <andy_0711@naver.com>
2026-04-02 01:36:04 +0900

    B2578 미완성

--- baekjoon/B2578.c
+++ baekjoon/B2578.c
@@ -1,44 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+// B2578 : 빙고
+#if 01
 int main(void)
 {
-	int arr[5][5] = { 0 };
-	int ans[5][5] = { 0 };
+	int arr[5][5] = {0};
+	int ans[25] = { 0 };
 	int cnt = 0;
-	int flag = 1;
-	int line = 1;
-	for (int i = 0; i < 5; ++i)
-	{
-		for (int j = 0; j < 5; ++j)
-		{
-			scanf("%d", &arr[i][j]);
-		}
-	}
-	for (int i = 0; i < 5; ++i)
-	{
-		for (int j = 0; j < 5; ++j)
-		{
-			scanf("%d", &ans[i][j]);
-		}
-	}
+	int line = 0;
 
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			scanf(" %d", &arr[i][j]);
+		}
+	}
+	for (int i = 0; i < 25; ++i)
+	{
+		scanf(" %d", &ans[i]);
+	}
 	for (int i = 0; i < 5; ++i)
 	{
 		for (int j = 0; j < 5; ++j)
 		{
 			for (int k = 0; k < 5; ++k)
 			{
-				for (int l = 0; l < 5; ++l)
+				if (arr[j][k] == ans[i * 5 + j])
 				{
-					if (arr[k][l] == ans[i][j])
-					{
-						arr[k][l] = 0;
-						cnt++;
-					}
-				}
 
+				}
 			}
 		}
 	}
-	
 }
+#endif
