e55170528d006ff43985d380bd45c85b1bfaf9e5
macusr0711 <andy_0711@naver.com>
2026-04-04 22:19:57 +0900

    수정 - 풀이변경

--- Baekjoon_Solve/B2578.c
+++ Baekjoon_Solve/B2578.c
@@ -5,18 +5,68 @@
 
 // B2578 : ºù°í
 #if 01
+
+int arr[5][5] = { 0 };
+int cnt = 0;
+int ans = 0;
+int line = 0;
+int zero = 0;
+int x = 0;
+int y = 0;
+
+int left_diag()
+{
+	zero = 0;
+	for (int j = 0; j < 5; ++j)
+	{
+		if (arr[j][j] == 0) zero++;
+		if (zero == 5) return 1;
+	}
+	return 0;
+}
+
+int right_diag()
+{
+	zero = 0;
+	for (int j = 0; j < 5; ++j)
+	{
+		if (arr[j][4 - j] == 0) zero++;
+		if (zero == 5) return 1;
+	}
+	return 0;
+}
+
+int row_x()
+{
+	x = 0;
+	for (int j = 0; j < 5; ++j)
+	{
+		zero = 0;
+		for (int k = 0; k < 5; ++k)
+		{
+			if (arr[j][k] == 0) zero++;
+			if (zero == 5) x++;
+		}
+	}
+	return x;
+}
+
+int col_y()
+{
+	y = 0;
+	for (int j = 0; j < 5; ++j)
+	{
+		zero = 0;
+		for (int k = 0; k < 5; ++k)
+		{
+			if (arr[k][j] == 0) zero++;
+			if (zero == 5) y++;
+		}
+	}
+	return y;
+}
 int main(void)
 {
-	int arr[5][5] = { 0 };
-	int ans[25] = { 0 };
-	int cnt = 0;
-	int line = 0;
-	int flag = 1;
-	int zero1 = 0;
-	int zero2 = 0;
-	int zero3 = 0;
-	int zero4 = 0;
-
 	for (int i = 0; i < 5; ++i)
 	{
 		for (int j = 0; j < 5; ++j)
@@ -26,15 +76,12 @@ int main(void)
 	}
 	for (int i = 0; i < 25; ++i)
 	{
-		scanf("%d", &ans[i]);
-	}
-	for (int i = 0; i < 25; ++i)
-	{
+		scanf("%d", &ans);
 		for (int j = 0; j < 5; ++j)
 		{
 			for (int k = 0; k < 5; ++k)
 			{
-				if (arr[j][k] == ans[i])
+				if (arr[j][k] == ans)
 				{
 					arr[j][k] = 0;
 					cnt++;
@@ -42,59 +89,12 @@ int main(void)
 			}
 		}
 		line = 0;
-		for (int j = 0; j < 5; ++j)
-		{
-			if (line == 3)
-			{
-				flag = 0;
-				break;
-			}
-			if (arr[j][j] == 0) zero3++;
-			if (zero3 == 5) line++;
-			if (line == 3)
-			{
-				flag = 0;
-				break;
-			}
-			if (arr[j][4 - j] == 0) zero4++;
-			if (zero4 == 5) line++;
-			if (line == 3)
-			{
-				flag = 0;
-				break;
-			}
-		}
-		zero3 = 0;
-		zero4 = 0;
-		for (int j = 0; j < 5; ++j)
-		{
-			for (int k = 0; k < 5; ++k)
-			{
-				if (line == 3)
-				{
-					flag = 0;
-					break;
-				}
-				if (arr[j][k] == 0) zero1++;
-				if (zero1 == 5) line++;
-				if (line == 3)
-				{
-					flag = 0;
-					break;
-				}
-				if (arr[k][j] == 0) zero2++;
-				if (zero2 == 5) line++;
-				if (line == 3)
-				{
-					flag = 0;
-					break;
-				}
-			}
-			if (flag == 0) break;
-			zero1 = 0;
-			zero2 = 0;
-		}
-		if (flag == 0) break;
+		line = left_diag();
+		line += right_diag();
+		line += row_x();
+		if (line >= 3) break;
+		line += col_y();
+		if (line >= 3) break;
 	}
 	printf("%d", cnt);
 }
