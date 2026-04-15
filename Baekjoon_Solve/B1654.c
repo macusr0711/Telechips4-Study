762f6c03ca43e0e49b4943275f059fcf2a451e3f
and_0711 <andy_0711@naver.com>
2026-04-15 20:22:01 +0900

    B1654.c 미완성

---
+++ Baekjoon_Solve/B1654.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if 01
+int *arr;
+int k = 0;
+int	n = 0;
+int l = 1;
+int r = 0;
+int m = 1;
+
+void inputdata()
+{
+	scanf("%d %d", &k, &n);
+	arr = (int*)calloc(1, sizeof(int) * k);
+	for (int i = 0; i < k; ++i)
+	{
+		scanf("%d", &arr[i]);
+	}
+}
+
+int compare(const int *a, const int *b)
+{
+	return (*a - *b);
+}
+
+int finddata(int k, int n)
+{
+	qsort(arr, k, sizeof(int), compare);
+	r = arr[k - 1];
+	while (1)
+	{
+		if (!(l < r)) break;
+		int temp = 0;
+		m = (l + r) / 2;
+		for (int i = 0; i < k; ++i)
+		{
+			temp += (arr[i] / m);
+		}
+		if (temp > n) l = m + 1;
+		else if (temp < n) r = m - 1;
+		else break;
+	}
+	return m;
+}
+
+int main(void)
+{
+	inputdata();
+	finddata(k, n);
+	printf("%d", m);
+}
+#endif
+
+/*
+802 = 200 x 4 
+743 = 200 x 3
+457 = 200 x 2
+539 = 200 x 2
+
+최대 길이로 11개
+
+*/
