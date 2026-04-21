ad194cb596bf3954f88d00460fd337dc88b74088
macusr0711 <andy_0711@naver.com>
2026-04-22 01:28:02 +0900

    B1654 집에서 수정 완료

--- Baekjoon_Solve/B1654.c
+++ Baekjoon_Solve/B1654.c
@@ -10,11 +10,12 @@ int	n = 0;
 int l = 1;
 int r = 0;
 int m = 1;
+int big = 1;
 
 void inputdata()
 {
 	scanf("%d %d", &k, &n);
-	arr = (int*)calloc(1, sizeof(int) * k);
+	arr = (int *)calloc(1, sizeof(int) * k);
 	for (int i = 0; i < k; ++i)
 	{
 		scanf("%d", &arr[i]);
@@ -26,40 +27,73 @@ int compare(const int *a, const int *b)
 	return (*a - *b);
 }
 
+void inputdata()
+{
+	scanf("%d %d", &k, &n);
+	arr = (int *)calloc(1, sizeof(int) * k);
+	for (int i = 0; i < k; ++i)
+	{
+		scanf("%d", &arr[i]);
+	}
+	qsort(arr, k, sizeof(int), compare);
+}
+
+
 int finddata(int k, int n)
 {
-	qsort(arr, k, sizeof(int), compare);
 	r = arr[k - 1];
 	while (1)
 	{
-		if (!(l < r)) break;
+		if (n == 1)
+		{
+			big = arr[0];
+			break;
+		}
+		if (l > r) break;
 		int temp = 0;
 		m = (l + r) / 2;
-		for (int i = 0; i < k; ++i)
+		for (int i = 0; i < k; i++)
 		{
 			temp += (arr[i] / m);
 		}
-		if (temp > n) l = m + 1;
-		else if (temp < n) r = m - 1;
-		else break;
-	}
-	return m;
-}
+		if ((temp >= n) && (big < m)) big = m;
 
-int main(void)
-{
-	inputdata();
-	finddata(k, n);
-	printf("%d", m);
-}
+		int finddata(int k, int n)
+		{
+			qsort(arr, k, sizeof(int), compare);
+			r = arr[k - 1];
+			while (1)
+			{
+				if (!(l < r)) break;
+				int temp = 0;
+				m = (l + r) / 2;
+				for (int i = 0; i < k; ++i)
+				{
+					temp += (arr[i] / m);
+				}
+				if (temp > n) l = m + 1;
+				else if (temp < n) r = m - 1;
+				else break;
+			}
+
+			return big;
+
+			return m;
+
+		}
+
+		int main(void)
+		{
+			inputdata();
+			finddata(k, n);
+
+			printf("%d", big);
+		}
 #endif
 
-/*
-802 = 200 x 4 
-743 = 200 x 3
-457 = 200 x 2
-539 = 200 x 2
 
-최대 길이로 11개
+		printf("%d", m);
+	}
+#endif
 
-*/
+
